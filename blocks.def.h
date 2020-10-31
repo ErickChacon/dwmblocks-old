325fab69c57fa2474a79ad837af0c351b5fd8af2
Erick Chacon <eral.th07@gmail.com>
2020-10-31 20:35:18 +0000

    ram add whitespace

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"RAM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	5,		0},
+	{"RAM: ", "free -h | awk '/^Mem/ { print $3\" / \"$2 }' | sed s/i//g",	5,		0},
 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
